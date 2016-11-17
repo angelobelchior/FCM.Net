368030b6f21b3be942a6d378379502406bc43010
Angelo Belchior <angelo.belchior@esx.com.br>
2016-11-17 17:23:48 -0200

    Initial Commit

---
+++ FCM.Net.Playground/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FCM.Net.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registrationId = "ID gerado quando o device é registrado no FCM";
+            var serverKey = "acesse https://console.firebase.google.com/project/MY_PROJECT/settings/cloudmessaging";
+
+            var sender = new Sender(serverKey);
+            var message = new Message
+            {
+                RegistrationIds = new List<string> { registrationId },
+                Notification = new Notification
+                {
+                    Title = "Test from FCM.Net",
+                    Body = $"Miguel@!{DateTime.Now.ToString()}"
+                }
+            };
+            var result = sender.SendAsync(message).Result;
+            WriteResult(result);
+
+            Console.Read();
+        }
+
+        private static void WriteResult(ResponseContent result)
+        {
+            Console.WriteLine($"StatusCode {result.StatusCode}");
+            Console.WriteLine($"ReasonPhrase {result.ReasonPhrase}");
+            if (result.MessageResponse == null) return;
+
+            Console.WriteLine($"MessageResponse.Success {result.MessageResponse.Success}");
+            Console.WriteLine($"MessageResponse.Failure {result.MessageResponse.Failure}");
+            Console.WriteLine($"MessageResponse.MulticastId {result.MessageResponse.MulticastId}");
+            Console.WriteLine($"MessageResponse.CanonicalIds {result.MessageResponse.CanonicalIds}");
+            Console.WriteLine($"MessageResponse.InternalError {result.MessageResponse.InternalError}");
+            Console.WriteLine($"MessageResponse.ResponseContent {result.MessageResponse.ResponseContent}");
+            if (result.MessageResponse.Results == null) return;
+
+            foreach (var item in result.MessageResponse.Results)
+            {
+                Console.WriteLine($"MessageResponse.Results.MessageId {item.MessageId}");
+                Console.WriteLine($"MessageResponse.Results.RegistrationId {item.RegistrationId}");
+                Console.WriteLine($"MessageResponse.Results.Error {item.Error}");
+            }
+        }
+    }
+}
---
+++ FCM.Net/Sender.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCM.Net
+{
+    /// <summary>
+    /// Send messages from your app server to client apps via Firebase Cloud Messaging
+    /// </summary>
+    public class Sender
+    {
+        private readonly string _endpoint = "https://fcm.googleapis.com/fcm/send";
+        private readonly string _contentType = "application/json";
+        private readonly string _serverKey;
+
+        /// <summary>
+        /// Initialize the Message Sender
+        /// </summary>
+        /// <param name="serverKey">Server Key. To access this information, go to https://console.firebase.google.com/project/<<MY_PROJECT>>/settings/cloudmessaging </param>
+        public Sender(string serverKey)
+        {
+            if (string.IsNullOrWhiteSpace(serverKey))
+                throw new ArgumentNullException(nameof(serverKey));
+
+            this._serverKey = serverKey;
+        }
+
+        /// <summary>
+        /// Send a message Async
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <returns>Response Content</returns>
+        public async Task<ResponseContent> SendAsync(Message message)
+        {
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+
+            using (var client = new HttpClient())
+            {
+                var requestContent = this.GetRequestContent(message);
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"key={this._serverKey}");
+
+                var response = await client.PostAsync(this._endpoint, requestContent);
+
+                var responseContent = await GetResponseContentAsync(response); ;
+                var result = new ResponseContent(response.StatusCode, response.ReasonPhrase, responseContent);
+
+                return result;
+            }
+        }
+
+        private HttpContent GetRequestContent(Message message)
+        {
+            string json = JsonConvert.SerializeObject(message, Newtonsoft.Json.Formatting.None,
+                            new JsonSerializerSettings
+                            {
+                                NullValueHandling = NullValueHandling.Ignore,
+                            });
+
+            var content = new StringContent(json, Encoding.UTF8, this._contentType);
+            return content;
+        }
+
+        private async Task<MessageResponse> GetResponseContentAsync(HttpResponseMessage response)
+        {
+            string json = string.Empty;
+            try
+            {
+                json = await response.Content.ReadAsStringAsync();
+                var messageResponse = JsonConvert.DeserializeObject<MessageResponse>(json);
+                return messageResponse;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return new MessageResponse { InternalError = ex.Message, ResponseContent = json };
+            }
+        }
+    }
+}
