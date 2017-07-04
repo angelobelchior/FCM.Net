d9efb27e9ec7ea2b4330f941d41e382924cae9ec
Angelo Luis <angelobelchior@hotmail.com>
2017-07-04 11:50:26 -0300

    Correção do static HttpClient e inclusão do Net Standard

--- FCM.Net.PCL/Properties/AssemblyInfo.cs
+++ FCM.Net.PCL/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FCM.Net.PCL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Biblioteca para facilitar envio de Push Notification usando o Firebase Cloud Messaging do Google")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Angelo Belchior")]
 [assembly: AssemblyProduct("FCM.Net.PCL")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
---
+++ PlaygroundCode/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FCM.Net.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var registrationId = "ID gerado quando o device é registrado no FCM";
+            //var serverKey = "acesse https://console.firebase.google.com/project/MY_PROJECT/settings/cloudmessaging";
+
+            var registrationId = "dG4rFnirWOE:APA91bE3COnsY-flnulPse4b4uKZOUDRpdOAe6DGTU_jWGtJt0P_hBXoN1tOa9Je4ZyAfA11OS3US0fZm6M7EljYipCY1f4MqjDLLvEltfe8_3aDnzwTxRbuw23HQ2JIY2ihXQXUvDym";
+            var serverKey = "AIzaSyC8dhbIHM0BEDextBkH1YRGwq2zWSPW2kk";
+
+
+            string title = "Teste .Net Core";
+            using (var sender = new Sender(serverKey))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    var message = new Message
+                    {
+                        RegistrationIds = new List<string> { registrationId },
+                        Notification = new Notification
+                        {
+                            Title = title,
+                            Body = $"Hello World@!{DateTime.Now.ToString()}"
+                        }
+                    };
+                    var result = sender.SendAsync(message).Result;
+                    WriteResult(result);
+                    Console.WriteLine($"Success: {result.MessageResponse.Success}");
+
+                    var json = "{\"notification\":{\"title\":\"mensagem em json\",\"body\":\"funciona!\"},\"to\":\"" + registrationId + "\"}";
+                    result = sender.SendAsync(json).Result;
+                    WriteResult(result);
+
+                    Thread.Sleep(1000);
+                }
+            }
+
+            Console.Read();
+        }
+
+        private static void WriteResult(ResponseContent result)
+        {
+            Console.WriteLine($"StatusCode: {result.StatusCode}");
+            Console.WriteLine($"ReasonPhrase: {result.ReasonPhrase}");
+            if (result.MessageResponse == null) return;
+
+            Console.WriteLine($"MessageResponse.Success: {result.MessageResponse.Success}");
+            Console.WriteLine($"MessageResponse.Failure: {result.MessageResponse.Failure}");
+            Console.WriteLine($"MessageResponse.MulticastId: {result.MessageResponse.MulticastId}");
+            Console.WriteLine($"MessageResponse.CanonicalIds: {result.MessageResponse.CanonicalIds}");
+            Console.WriteLine($"MessageResponse.InternalError: {result.MessageResponse.InternalError}");
+            Console.WriteLine($"MessageResponse.ResponseContent: {result.MessageResponse.ResponseContent}");
+            if (result.MessageResponse.Results == null) return;
+
+            foreach (var item in result.MessageResponse.Results)
+            {
+                Console.WriteLine($"MessageResponse.Results.MessageId: {item.MessageId}");
+                Console.WriteLine($"MessageResponse.Results.RegistrationId: {item.RegistrationId}");
+                Console.WriteLine($"MessageResponse.Results.Error: {item.Error}");
+            }
+            Console.WriteLine(new string('-', 20));
+        }
+    }
+}
