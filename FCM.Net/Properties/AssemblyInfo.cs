4a918ab4b978cf954296188f8d106d85cd86e4d6
Angelo Luis <angelobelchior@hotmail.com>
2017-07-04 16:27:41 -0300

    inclusão do nuget

--- FCM.Net/Properties/AssemblyInfo.cs
+++ FCM.Net/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FCM.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Biblioteca para facilitar envio de Push Notification usando o Firebase Cloud Messaging do Google")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Angelo Belchior")]
 [assembly: AssemblyProduct("FCM.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ FCM.Net/nuget/nuget-pack.bat
@@ -0,0 +1,1 @@
+nuget\nuget.exe pack FCM.Net.csproj -Prop Configuration=Release
